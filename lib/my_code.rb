7abdab01a46f02561a83ff33c4eb2cffc09ee0ff
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-31 01:12:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,19 +13,22 @@ end
 def reduce(array,starting_point=0)
 i=0
 while i<array.length do
-  yield()
+  yield(array[i])
+  i+=1
 end
+if starting_point==
 end
 
-def i_hear_a_sick_sound(passengers_sounds)
-  i = 0 # set up a i for the enumeration of the passengers collection
-  while i < passengers_sounds.length do # a loop for each passenger
-    # Stop enumerating and return true if any passenger is
-    # coughing or sneezing
-    if (passengers_sounds[i] == "coughing" || passengers_sounds[i] == "sneezing")
-      return true
-    end
-    i += 1
-  end
-  return false
-end
+
+# def i_hear_a_sick_sound(passengers_sounds)
+#   i = 0 # set up a i for the enumeration of the passengers collection
+#   while i < passengers_sounds.length do # a loop for each passenger
+#     # Stop enumerating and return true if any passenger is
+#     # coughing or sneezing
+#     if (passengers_sounds[i] == "coughing" || passengers_sounds[i] == "sneezing")
+#       return true
+#     end
+#     i += 1
+#   end
+#   return false
+# end
